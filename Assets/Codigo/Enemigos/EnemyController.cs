82369d2ebece7fd1edced67b7651733c5677fa87
Juan Cordero <juanignaciocordero3@gmail.com>
2026-03-14 03:34:53 -0600

    Correcion de varios errores, mejora del personaje con sus animaciones, mejora de los enemigos, pinchos funcionales y sistema de 3 vidas funcional

---
+++ Assets/Codigo/Enemigos/EnemyController.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class EnemyController : MonoBehaviour
+
+{
+
+    public Transform player;
+
+    public float detectionRadius = 5.0f;
+
+    public float speed = 2.0f;
+
+    private Rigidbody2D rb;
+
+    private Vector2 movement;
+
+    private bool JugadorVivo;
+
+    void Start()
+
+    {
+        JugadorVivo = true;
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if(JugadorVivo)
+        {
+            Movimiento();
+        }
+    }
+
+    private void Movimiento()
+    {
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        if (distanceToPlayer < detectionRadius)
+        {
+            Vector2 direction = (player.position - transform.position).normalized;
+
+            movement = new Vector2(direction.x, 0);
+        }
+        else
+        {
+            movement = Vector2.zero;
+        }
+
+        rb.MovePosition(rb.position + movement * speed * Time.deltaTime);
+    }
+     private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            Vector2 direccionDanio = new Vector2(transform.position.x, 0);
+            PlayerController playerScript = collision.gameObject.GetComponent<PlayerController>();
+            playerScript.RecibeDanio(direccionDanio, 1);
+            JugadorVivo = !playerScript.muerto;
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawWireSphere(transform.position, detectionRadius);
+    }
+
+}
---
+++ Assets/Codigo/Jugador/PlayerController.cs
@@ -0,0 +1,111 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 2f;
+    public int vida = 3;
+    public float fuerzaSalto = 5f;
+    public float fuerzaRebote = 10f;
+    public float longitudRaycast = 0.1f;
+    public LayerMask capaSuelo;
+    public bool enSuelo;
+    private bool recibiendoDanio;
+    public bool muerto;
+    private Rigidbody2D rb;
+    public Animator anim;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        if(!muerto)
+        {
+        Movimiento();
+
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.down,longitudRaycast, capaSuelo);
+        enSuelo = hit.collider != null;
+
+        if(enSuelo && Input.GetKeyDown(KeyCode.Space) && !recibiendoDanio)
+        {
+            rb.AddForce(new Vector2(0f, fuerzaSalto), ForceMode2D.Impulse);
+        }
+
+  
+        }
+        
+        anim.SetBool("ensuelo", enSuelo);
+        anim.SetBool("recibeDanio", recibiendoDanio);
+        anim.SetBool("muerto", muerto);
+    }
+
+    public void Movimiento()
+    {
+        float velocidadX = Input.GetAxis("Horizontal")*Time.deltaTime*velocidad;
+
+        anim.SetFloat("Movement", velocidadX*velocidad);
+
+        if(velocidadX < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        if (velocidadX > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        Vector3 posicion = transform.position;
+
+        if(!recibiendoDanio)
+        transform. position = new Vector3(velocidadX + posicion.x, posicion.y, posicion.z);  
+    }
+    public void RecibeDanio(Vector2 direccion, int cantDanio)
+    {
+        if(!recibiendoDanio)
+        {
+        recibiendoDanio = true;
+        vida -= cantDanio;
+        if(vida<=0)
+        {
+            muerto = true;
+        }
+
+        if(!muerto)
+        {
+            Vector2 rebote = new Vector2(transform.position.x - direccion.x, 0.2f).normalized;
+             rb.AddForce(rebote * fuerzaRebote, ForceMode2D.Impulse);  
+        }
+
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("PinchosNormales"))
+        {
+            Vector2 direccionDanio = collision.transform.position;
+            RecibeDanio(direccionDanio, 1);
+        }
+
+        if(collision.gameObject.CompareTag("Pinchos_Insta_Kill"))
+        {
+            muerto = true;
+        }
+    }
+
+    public void DesactivaDanio()
+    {
+     recibiendoDanio = false;
+     rb.velocity = Vector2.zero;
+     
+    }
+        void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + Vector3.down * longitudRaycast);
+    }
+}
