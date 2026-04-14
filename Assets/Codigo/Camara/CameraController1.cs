919eb294e72d234b8a562e4857f905e4b80974f1
Juan Cordero <juanignaciocordero3@gmail.com>
2026-04-14 08:46:14 -0600

    Cambios en mapa antes de merge

--- Assets/Codigo/Camara/CameraController1.cs
+++ Assets/Codigo/Camara/CameraController1.cs
@@ -6,7 +6,7 @@ public class CameraController1 : MonoBehaviour
 {
 
 
-    private Vector3 offset = new Vector3(0f , 0f , -10f);
+    private Vector3 offset = new Vector3(5f , 0f , -10f);
     private float smoothTime = 0.25f;
     private Vector3 velocity = Vector3.zero;
 
